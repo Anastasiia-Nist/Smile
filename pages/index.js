43ac2b0cdec7b1dbbd863eda2a509f867fb8c6e5
Anastasia Nistratova <a.nistratova14@gmail.com>
2023-06-16 14:58:44 +0300

    feat: add validation & fix style

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,8 @@ const mobileNavList = document.querySelectorAll(".nav-mobile__item");
 const header = document.querySelector(".header");
 const loginForm = document.forms.LoginForm;
 const passwordControl = document.querySelector(".password-control");
+const inputListLogin = loginForm.querySelectorAll("input");
+let userInfo = {};
 
 const openPopup = (popup) => {
   popup.classList.add("popup_opened");
@@ -39,11 +41,16 @@ popups.forEach((popup) => {
   });
 });
 
-const preventDefault = (evt) => {
+const submitForm = (evt) => {
   evt.preventDefault();
+  getInputValues();
   closePopup(popupLogin);
 };
-
+function getInputValues() {
+  inputListLogin.forEach((input) => {
+    userInfo[input.id] = input.value;
+  });
+}
 function blockedScroll() {
   document.body.classList.add("page-lock");
 }
@@ -52,7 +59,7 @@ function anBlockedScroll() {
 }
 
 function showPassword(evt) {
-  const input = document.getElementById("password-input");
+  const input = document.getElementById("password");
   if (input.getAttribute("type") == "password") {
     evt.target.classList.add("view");
     input.setAttribute("type", "text");
@@ -63,13 +70,55 @@ function showPassword(evt) {
   //return false;
 }
 
+//валидация
+function showError(inputElement) {
+  const errorElement = document.getElementById(`${inputElement.id}-error`);
+  errorElement.textContent = inputElement.validationMessage;
+  errorElement.classList.add(`form__input-error_active`);
+}
+function hiddenError(inputElement) {
+  const errorElement = document.getElementById(`${inputElement.id}-error`);
+  errorElement.classList.remove(`form__input-error_active`);
+  errorElement.textContent = "";
+}
+function hiddenAllErrors() {
+  inputListLogin.forEach((inputElement) => {
+    hiddenError(inputElement);
+  });
+}
+function toggleButtonState(form) {
+  const isFormValid = form.checkValidity();
+  const submitButton = form.querySelector(".form__button");
+  submitButton.toggleAttribute("disabled", !isFormValid);
+}
+
+function isValid(inputElement) {
+  if (!inputElement.validity.valid) {
+    showError(inputElement);
+  } else {
+    hiddenError(inputElement);
+  }
+}
+
 //слушатели на странице
 btnsStarted.forEach((btn) => {
-  btn.addEventListener("click", () => openPopup(popupLogin));
+  btn.addEventListener("click", () => {
+    openPopup(popupLogin);
+    hiddenAllErrors();
+  });
 });
 btnBurger.addEventListener("click", () => openPopup(popupBurger));
 mobileNavList.forEach((item) => {
   item.addEventListener("click", () => closePopup(popupBurger));
 });
-loginForm.addEventListener("submit", preventDefault);
+loginForm.addEventListener("submit", submitForm);
+
 passwordControl.addEventListener("click", showPassword);
+
+inputListLogin.forEach((inputElement) => {
+  inputElement.addEventListener("input", () => {
+    isValid(inputElement);
+    toggleButtonState(loginForm);
+  });
+  toggleButtonState(loginForm);
+});
